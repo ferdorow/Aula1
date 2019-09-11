4d195949535763a7ddca6774cf67dc07e74f9413
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-11 14:45:38 -0300

    commit

---
+++ VerificarNome/VerificarNome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarNome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informar uma sequencia de numeros:");
+            var textContent = Console.ReadLine();
+
+            Console.WriteLine(textContent.Replace("3", "Banana"));
+            Console.ReadKey();
+           
+        }
+    }
+}
