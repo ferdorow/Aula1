28204376d1b1c39836e1ade06e4197fa67147be1
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-11 17:06:38 -0300

    commit

---
+++ CompactarTexto/CompactarTexto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompactarTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var PrimeiroTexto = "Oi meu nome Fernando e logo eu Fernando tenho vontade de codar muito porque eu Fernando gosto de codar sempre";
+
+                Console.WriteLine(PrimeiroTexto);
+                Console.WriteLine($"Texto contem:")
+;        }
+    }
+}
---
+++ StringTypesConcat/StringTypesConcat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTypesConcat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var teste = "alguma coisa mais " +
+                "alguma informação aqui";
+
+            var teste1 = Console.ReadLine();
+            var teste2 = Console.ReadLine();
+            var teste3 = teste1 + "\r\n" + teste2;
+            
+            Console.WriteLine(teste3);
+
+            Console.WriteLine(teste);
+            Console.ReadKey();
+
+              
+        }
+    }
+}
