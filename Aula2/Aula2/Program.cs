5c111af9b8313ffd74f68b713ddf685f3fdd87a9
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-11 14:24:06 -0300

    commit

--- Aula2/Aula2/Program.cs
+++ Aula2/Aula2/Program.cs
@@ -10,6 +10,18 @@ namespace Aula2
     {
         static void Main(string[] args)
         {
+            //Iniciando uma constante de um numero inteiro
+            const int testeConst = 999;
+
+            var teste1 = "String"; //Texto
+            var teste2 = 'C'; //char
+            var teste3 = 9; //int
+            var teste4 = 3.8; //double
+            var teste5 = 0x0f; //Hex
+
+            Console.WriteLine(testeConst);
+            Console.Readkey();
+
         }
     }
 }
---
+++ calculo area/Calculo Area/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Declaração das variáveis
+            double b, h, area;
+            //mensagem para digitar a base
+            Console.Write("DIGITE O VALOR DA BASE (cm): ");
+            //variável b recebe o valor convertido para
+            //o tipo correto que no caso é double
+            b = Convert.ToDouble(Console.ReadLine());
+            //mensagem para digitar a base
+            Console.Write("DIGITE O VALOR DA ALTURA (cm): ");
+            //variável h recebe o valor convertido para
+            //o tipo correto que no caso também é double
+            h = Convert.ToDouble(Console.ReadLine());
+            //Área recebe o resultado da "fórmula"
+            area = b * h / 2;
+            //exibo o resultado
+            Console.WriteLine("A área do triangulo é: " + area.ToString("F2") + " (cm²)");
+            Console.WriteLine("Pressione qualquer tecla para sair!");
+            Console.ReadKey();
+        }
+    }
+}
