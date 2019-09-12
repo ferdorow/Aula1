95601f2be3fe21e5c87097cf164aa3dfc7836ff6
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-12 09:00:23 -0300

    comm

---
+++ LacosDeRepeticao/UtilizandoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+
+            while (count <1000000)
+
+            {
+                Console.WriteLine($"numero:{count}");
+                count++;
+                
+                
+                Console.WriteLine("Terminou o loop");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
