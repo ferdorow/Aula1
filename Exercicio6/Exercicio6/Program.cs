346ca1a4bec24ff425ecf1e22ebb2fc6c3625ff4
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-13 17:22:00 -0300

    comm

--- Exercicio6/Exercicio6/Program.cs
+++ Exercicio6/Exercicio6/Program.cs
@@ -10,14 +10,14 @@ namespace Exercicio6
     {
         static void Main(string[] args)
         {
-            string textoInicial;
-            string textoFinal;
+            string textoInicial; //texto de entrada
+            string textoFinal;  //texto de saída, já com a troca
 
-            Console.WriteLine("Digite um texto");
-            textoInicial = Console.ReadLine().ToLower();
-            textoFinal = textoInicial.Replace("banana", "laranja");
+            Console.WriteLine("Digite um texto"); 
+            textoInicial = Console.ReadLine().ToLower(); //tolower converte letra maiuscula em minuscula
+            textoFinal = textoInicial.Replace("banana", "laranja"); //replace substitui um por outro
 
-            Console.WriteLine (textoFinal);
+            Console.WriteLine (textoFinal); //escrevendo o texto final
 
             Console.ReadKey();
            
