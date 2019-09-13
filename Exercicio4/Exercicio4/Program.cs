539f54ef4f253b1d01a38b66c9b9eacf0c29a92c
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-13 15:27:09 -0300

    commi

---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoCompleto;
+
+            Console.WriteLine("Digite um texto");
+            textoCompleto = Console.ReadLine();
+            Console.WriteLine($"O texto possui {textoCompleto.Length} caracteres.");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
