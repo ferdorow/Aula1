53f7715b2ae25a2da9702bc4b82e4f3b797620b6
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-13 14:08:58 -0300

    comm

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja bem vindo ao fim de semana! O chopp está liberado!");
+            Console.ReadKey();
+
+        }
+    }
+}
