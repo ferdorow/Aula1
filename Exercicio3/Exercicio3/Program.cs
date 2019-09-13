bea32a5904a4d8e0d12496aabb22bbc5fb425a2c
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-13 15:01:44 -0300

    COmm

---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            Console.WriteLine("Qual o seu nome?");
+            nome = Console.ReadLine();
+            Console.WriteLine("Qual a sua idade?");
+            idade = int.Parse(Console.ReadLine());
+                       
+            if (idade >= 18)
+                Console.WriteLine($"Parabéns {nome} você ja está na fase adulta ");
+
+            else
+
+                Console.WriteLine($"Calma {nome} tudo ao seu tempo logo você terá 18 anos de idade.");
+
+            Console.ReadKey();
+
+        
+
+
+
+
+
+        }       
+
+
+            
+    }
+}
