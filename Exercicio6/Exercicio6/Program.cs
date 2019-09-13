e5d5590609226f212569c16393599d3da0d9a4d5
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-13 17:11:33 -0300

    comm

---
+++ Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoInicial;
+            string textoFinal;
+
+            Console.WriteLine("Digite um texto");
+            textoInicial = Console.ReadLine().ToLower();
+            textoFinal = textoInicial.Replace("banana", "laranja");
+
+            Console.WriteLine (textoFinal);
+
+            Console.ReadKey();
+           
+
+        }
+    }
+}
