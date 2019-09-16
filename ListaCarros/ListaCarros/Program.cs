d560ac462e5b8ba48b0b1a6f5cfcfc01b172a371
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-16 14:35:00 -0300

    comm

--- ListaCarros/ListaCarros/Program.cs
+++ ListaCarros/ListaCarros/Program.cs
@@ -10,29 +10,29 @@ namespace ListaCarros
     {
         static void Main(string[] args)
         {
-            CarSplitList();
+            ConsultaDeCarros();
         }
-        private static void CarSplitList()
+        private static void ConsultaDeCarros()
         {
             //variaveis contendo o conteudo da lista e o conteudo da lista separado
             var conteudo = "carro:Gol,marca:volkswagen,ano:2000;carro:Jetta,marca:volkswagen,ano:2012;carro:Sportage,marca:Kia,ano:2011;carro:Hb20,marca:hyundai,ano:2015";
             var listaDeInformacoes = conteudo.Split(';');
 
-            Console.WriteLine("Carros cadastrados no sistema"); 
+            Console.WriteLine("Carros cadastrados no sistema:");
 
             foreach (var item in listaDeInformacoes)
             {
-                Console.WriteLine(item.Split(',')[0]);
+                Console.WriteLine(item.Split(',')[0]); // escreve os nomes da lista na posiçao 0
             }
 
-            Console.WriteLine("Informe um nome do sistema:");
+            Console.WriteLine("Informe um nome de carro do sistema:");
             var nomeBusca = Console.ReadLine();
 
             foreach (var item in listaDeInformacoes)
             {
-                var informacoesSplit = item.Split(',');
+                var informacoesSplit = item.Split(',');    //separando na virgula
 
-                var carro = informacoesSplit[0].Split(':')[1];
+                var carro = informacoesSplit[0].Split(':')[1];         //separando nos :
                 var marca = informacoesSplit[1].Split(':')[1];
                 var ano = informacoesSplit[2].Split(':')[1];
 
