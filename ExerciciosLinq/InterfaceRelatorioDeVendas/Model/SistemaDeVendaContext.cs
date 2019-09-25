3a8f1c0da45e2af27271aff60a89c961aa1e707a
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-25 15:33:33 -0300

    co

---
+++ ExerciciosLinq/InterfaceRelatorioDeVendas/Model/SistemaDeVendaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceRelatorioDeVendas.Model
+{
+    public class SistemaDeVendaContext
+    {
+        public int IdContadorCarros { get; set; } = 1;
+    }
+}
