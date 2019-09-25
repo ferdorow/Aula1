b82d8b548cc416b7dc64af598ae69cff6117f206
Fernando Dorow <fernando.dorow@outlook.com>
2019-09-25 10:45:01 -0300

    comm

---
+++ FuncoesLinq/AggregateLinq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregateLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listaDeNomes = new List<string>()
+            {
+                "Felipe",
+                "Giomar",
+                "Euricledson",
+                "Adagoberto",
+                "Ezebio",
+                "irineudson"
+            };
+
+            //No Aggregate temos o nome atual que estamos lendo e o proximo nome da lista
+            //no passo seguinte temos o nome 'proximo' vira o atual e pega o proximo da lista
+            var texto = listaDeNomes.Aggregate((nome1, nome2) => nome1 + "; " + nome2);
+
+            Console.WriteLine(texto);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FuncoesLinq/SumLinq/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomarInteirosPrimitivos();
+
+            SomaInteirosLista();
+
+            SomaBalasListaDeCriancas();
+
+            Console.ReadKey();
+
+        }
+        /// <summary>
+        /// Metodo que mostra numeros inteiros por um tipo primitivo
+        /// </summary>
+        private static void SomarInteirosPrimitivos()
+        {
+            //quando utilizamos "[]" em um tipo "primitivo" podemos criar uma colecao desses valores
+            //Aqui carregamos nossa coeleção de valores e
+            //... no "int[5]" alocamos memoria que vamos ocupar
+            int[] colecaoInteiros = new int[5] { 1, 2, 3, 4, 5 };
+
+            //Aqui somamos os valores em nossa lista
+            Console.WriteLine(colecaoInteiros.Sum());
+        }
+        /// <summary>
+        /// Metodo que soma valores de [um em um] em uma lista
+        /// </summary>
+        private static void SomaInteirosLista()
+        {
+            List<int> listaDeInteiros = new List<int>()
+            {
+                //nossa coleção de valores
+                1,2,3,4,5,6,7,8,9,10,11,12
+            };
+
+            Console.WriteLine(listaDeInteiros.Sum());
+        }
+        private static void SomaBalasListaDeCriancas()
+        {
+            List<Crianca> criancas = new List<Crianca>()
+            {
+                new Crianca
+                {
+                    Nome = "Joazinho",
+                    Balas = 9
+                },
+               new Crianca
+               {
+               Nome = "Pedrinho",
+               Balas = 68
+
+               }
+            };
+            Console.WriteLine("Quantidade total de balas que as criancinhas levaram da venda");
+                                        //soma nossa quantidade de balas
+            Console.WriteLine(criancas.Sum(x => x.Balas));
+        }
+    }
+
+}
+
