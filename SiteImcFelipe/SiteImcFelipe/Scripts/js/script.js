6541699521b3f9f614058e75981cd44e7b77bb6e
Fernando Dorow <fernando.dorow@outlook.com>
2019-10-28 17:22:48 -0300

    commit dia 28/10

---
+++ SiteImcFelipe/SiteImcFelipe/Scripts/js/script.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () { }
+
+    $('button[name="btncalcular"]').click(function ()
+
+        var informacoes = $('form[name="formic"]').seriealize();
+        var url = "http://localhost:54967/api/CalculoImc?";
+
+        
+          
+
+           
+        });
+        
+   });
+
+});
---
+++ SolucaoListaDeNomes/SolucaoListaDeNomes/Controllers/ListaNomesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SolucaoListaDeNomes.Models;
+
+namespace SolucaoListaDeNomes.Controllers
+{
+    public class ListaNomesController : ApiController
+    {
+        List<Pessoa> listaDePessoas = new List<Pessoa>()
+        {
+            new Pessoa(){ Nome = "Felipe", Idade = 55},
+            new Pessoa(){ Nome = "Cliber", Idade = 22},
+            new Pessoa(){ Nome = "Giomar", Idade = 18},
+            new Pessoa(){ Nome = "Zeca", Idade = 64},
+            new Pessoa(){ Nome = "Gumercindo", Idade = 33},
+            new Pessoa(){ Nome = "Leoncio", Idade = 16},
+            new Pessoa(){ Nome = "Fulanex", Idade = 44},
+            new Pessoa(){ Nome = "Ciclanex", Idade = 80},
+            new Pessoa(){ Nome = "Beltranex", Idade = 21},
+            new Pessoa(){ Nome = "Zeca", Idade = 52},
+
+
+        };
+        public List<Pessoa> Get()
+        {            
+            return listaDePessoas;
+        }
+
+        public List<Pessoa> Post(Pessoa values)
+        {
+            listaDePessoas.Add(values);
+            return listaDePessoas;
+
+        }
+    }
+}
