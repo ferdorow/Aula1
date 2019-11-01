311723bd9513f978de6936708f54a1f295da0f03
Fernando Dorow <fernando.dorow@hbsis.com.br>
2019-11-01 17:29:17 -0300

    sexta

---
+++ FirstCoreWebApi/FirstCoreWebApi/Models/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreWebApi.Models
+{
+    public class Carro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Model { get; set; }
+
+    }
+}
---
+++ FirstCoreWebApi/FirstCoreWebApi/Models/FirstCodeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstCoreWebApi.Models
+{
+    public class FirstCodeContext : DbContext
+    {
+        public FirstCodeContext(DbContextOptions<FirstCodeContext>options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Carro> Carros { get; set; }
+    }
+}
