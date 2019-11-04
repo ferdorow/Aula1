9141cdad702604d380c1a6b6bd1568ddf1f336bb
Fernando Dorow <fernando.dorow@outlook.com>
2019-11-04 16:34:35 -0300

    Commit exercicios 04/11

---
+++ WebImoveis/WebImoveis/Model/Controle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebImoveis.Model
+{
+    public class Controle
+    {
+        public bool Ativo { get; set; } = true;
+        public int UsuInc { get; set; }
+        public int UsuAlt { get; set; }
+        public DateTime DataInc { get; set; } = DateTime.Now;
+        public DateTime DataAlt { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ WebImoveis/WebImoveis/Model/RegistroContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebImoveis.Model
+{
+    public class RegistroContext : DbContext
+    {
+        public RegistroContext(DbContextOptions<RegistroContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Imovel> Imoveis { get; set; }
+        public DbSet<Proprietario> Proprietarios { get; set; }
+    }
+
+}
